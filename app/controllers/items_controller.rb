e1e68ef3e247d9e0ef0e7c22af12a0d8fafbf8b5
kcua <kcuaresma17@gmail.com>
2025-12-06 14:54:38 +0000

    Removed edit permission for normal users

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_item, only: %i[ show edit update destroy ]
+  before_action :require_manager, only: [:new, :create, :edit, :update, :destroy]
 
   # GET /items or /items.json
   def index
@@ -20,7 +21,7 @@ class ItemsController < ApplicationController
   def edit
   end
 
-  # POST /items or /items.json
+  # POST /items
   def create
     @item = Item.new(item_params)
 
@@ -35,7 +36,7 @@ class ItemsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /items/1 or /items/1.json
+  # PATCH/PUT /items/1
   def update
     respond_to do |format|
       if @item.update(item_params)
@@ -48,7 +49,7 @@ class ItemsController < ApplicationController
     end
   end
 
-  # DELETE /items/1 or /items/1.json
+  # DELETE /items/1
   def destroy
     @item.destroy!
 
@@ -65,5 +66,11 @@ class ItemsController < ApplicationController
 
     def item_params
       params.require(:item).permit(:name, :sku, :quantity, :price, :description, :category_id)
+    end
+
+    def require_manager
+      unless current_user.manager?
+        redirect_to items_path, alert: "You do not have permission to modify items."
+      end
     end
 end
